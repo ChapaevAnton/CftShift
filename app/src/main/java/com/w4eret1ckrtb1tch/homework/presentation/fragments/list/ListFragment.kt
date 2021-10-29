772a35099a735d1e218131f608d17b3b1821bebb
ChapaevAnton <virsus2009@gmail.com>
2021-10-29 16:03:58 +0700

    Homework 27.10.21 saving state
    * added view model

--- app/src/main/java/com/w4eret1ckrtb1tch/homework/presentation/fragments/list/ListFragment.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/homework/presentation/fragments/list/ListFragment.kt
@@ -2,20 +2,18 @@ package com.w4eret1ckrtb1tch.homework.presentation.fragments.list
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.google.android.material.snackbar.Snackbar
 import com.w4eret1ckrtb1tch.homework.R
-import com.w4eret1ckrtb1tch.homework.data.db.AppDataBase
 import com.w4eret1ckrtb1tch.homework.databinding.FragmentListBinding
-import com.w4eret1ckrtb1tch.homework.domain.model.ContactEntity
 import com.w4eret1ckrtb1tch.homework.presentation.adapters.ContactAdapter
 import com.w4eret1ckrtb1tch.homework.presentation.adapters.RecyclerDecoration
 
@@ -25,7 +23,24 @@ class ListFragment : Fragment(R.layout.fragment_list) {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
     private val decorator by lazy { RecyclerDecoration(sidePagingDp = 8, bottomPagingDp = 8) }
+    private val viewModel: ListViewModel by viewModels()
     private lateinit var contactAdapter: ContactAdapter
+    private var mPermissionResult: ActivityResultLauncher<String>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mPermissionResult = registerForActivityResult(
+            RequestPermission()
+        ) { result: Boolean ->
+            if (result) {
+                Log.d("TEST", "Permission granted...!")
+                viewModel.loadContacts()
+            } else {
+                Log.d("TEST", "Permission denied...!")
+                showDescription("Permission denied")
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,7 +54,6 @@ class ListFragment : Fragment(R.layout.fragment_list) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         contactAdapter = ContactAdapter(onClick = { })
-        // TODO: 29.10.2021 set contacts to adapter
         binding.recyclerView.addItemDecoration(decorator)
         binding.recyclerView.adapter = contactAdapter
         binding.toolbar.setOnMenuItemClickListener { menuItem ->
@@ -52,7 +66,10 @@ class ListFragment : Fragment(R.layout.fragment_list) {
             }
 
         }
-        checkPermission()
+        viewModel.checkPermission()
+        viewModel.getContacts()
+            .observe(viewLifecycleOwner) { contacts -> contactAdapter.contactEntities = contacts }
+        viewModel.getPermission().observe(viewLifecycleOwner) { showPermission() }
     }
 
     override fun onDestroyView() {
@@ -60,93 +77,31 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         super.onDestroyView()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CONTACTS_REQUEST &&
-            grantResults.isNotEmpty() &&
-            grantResults[0] == PackageManager.PERMISSION_GRANTED
-        ) {
-            Log.d("TAG", "onRequestPermissionsResult: ok")
-            getContacts()
-        } else {
-            showDescription("Permission denied")
-        }
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == CONTACTS_REQUEST &&
+//            grantResults.isNotEmpty() &&
+//            grantResults[0] == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            Log.d("TAG", "onRequestPermissionsResult: ok")
+//            viewModel.loadContacts()
+//        } else {
+//            showDescription("Permission denied")
+//        }
+//    }
+
+    private fun showPermission() {
+        mPermissionResult?.launch(Manifest.permission.READ_CONTACTS, null)
     }
 
     private fun showDescription(description: String) {
         Snackbar.make(binding.root, description, Snackbar.LENGTH_SHORT).setMaxInlineActionWidth(
             resources.getDimensionPixelSize(R.dimen.design_snackbar_action_inline_max_width)
         ).show()
-    }
-
-    private fun checkPermission() {
-        if (ActivityCompat.checkSelfPermission(
-                requireActivity(),
-                Manifest.permission.READ_CONTACTS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(
-                arrayOf(Manifest.permission.READ_CONTACTS),
-                CONTACTS_REQUEST
-            )
-        } else {
-            Log.d("TAG", "checkPermission: ok")
-            getContacts()
-        }
-    }
-
-    private fun getContacts() {
-        val contacts = mutableListOf<ContactEntity>()
-        val uri: Uri = ContactsContract.Contacts.CONTENT_URI
-        val sort = "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} ASC"
-        val cursor = requireActivity().contentResolver.query(uri, null, null, null, sort)
-        cursor?.let {
-            if (cursor.count > 0) {
-                while (cursor.moveToNext()) {
-                    val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
-                    val name =
-                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                    val uriPhone = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
-                    val selection = "${ContactsContract.CommonDataKinds.Phone.CONTACT_ID} =?"
-                    val phoneCursor =
-                        requireActivity().contentResolver.query(
-                            uriPhone,
-                            null,
-                            selection,
-                            arrayOf(id),
-                            null
-                        )
-                    phoneCursor?.let {
-                        if (phoneCursor.count > 0) {
-                            Log.d("TAG", "getContacts: ${phoneCursor.count}")
-                            if (phoneCursor.moveToNext()) {
-                                val number =
-                                    phoneCursor.getString(
-                                        phoneCursor.getColumnIndex(
-                                            ContactsContract.CommonDataKinds.Phone.NUMBER
-                                        )
-                                    )
-                                Log.d("TAG", "getContacts: $name $number ${phoneCursor.position}")
-                                when (phoneCursor.getInt(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE))) {
-                                    ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE -> {
-                                        val contact = ContactEntity(name = name, number = number)
-                                        contacts.add(contact)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    phoneCursor?.close()
-                }
-            }
-        }
-        cursor?.close()
-        saveState(contacts)
-        contactAdapter.contactEntities = restoreState()
     }
 
     companion object {
@@ -159,13 +114,4 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         const val CONTACTS_REQUEST = 100
     }
 
-    fun saveState(contacts: List<ContactEntity>) {
-        val database = AppDataBase.getDatabase(requireActivity())
-        database.contactsDao().insertAll(contacts)
-    }
-
-    fun restoreState(): List<ContactEntity> {
-        val database = AppDataBase.getDatabase(requireActivity())
-        return database.contactsDao().selectAll()
-    }
 }
---
+++ app/src/main/java/com/w4eret1ckrtb1tch/homework/presentation/fragments/list/ListViewModel.kt
@@ -0,0 +1,103 @@
+package com.w4eret1ckrtb1tch.homework.presentation.fragments.list
+
+import android.Manifest
+import android.app.Application
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.w4eret1ckrtb1tch.homework.data.db.AppDataBase
+import com.w4eret1ckrtb1tch.homework.domain.model.ContactEntity
+import com.w4eret1ckrtb1tch.homework.presentation.fragments.SingleLiveEvent
+
+class ListViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val database = AppDataBase.getDatabase(application.applicationContext)
+
+    private val contacts = MutableLiveData<List<ContactEntity>>()
+    fun getContacts(): LiveData<List<ContactEntity>> = contacts
+    private val showPermission = SingleLiveEvent<Unit>()
+    fun getPermission(): LiveData<Unit> = showPermission
+
+    fun checkPermission() {
+        if (isPermission()) {
+            showPermission.value = Unit
+        } else {
+            contacts.value = restoreState()
+        }
+    }
+
+    fun loadContacts() {
+        loadContactsToDataBase()
+        contacts.value = restoreState()
+    }
+
+    private fun isPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            getApplication(),
+            Manifest.permission.READ_CONTACTS
+        ) != PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun saveState(contacts: List<ContactEntity>) {
+        database.contactsDao().insertAll(contacts)
+    }
+
+    private fun restoreState(): List<ContactEntity> {
+        return database.contactsDao().selectAll()
+    }
+
+    private fun loadContactsToDataBase() {
+        val contacts = mutableListOf<ContactEntity>()
+        val uri: Uri = ContactsContract.Contacts.CONTENT_URI
+        val sort = "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} ASC"
+        val cursor =
+            getApplication<Application>().contentResolver.query(uri, null, null, null, sort)
+        cursor?.let {
+            if (cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                    val name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    val uriPhone = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                    val selection = "${ContactsContract.CommonDataKinds.Phone.CONTACT_ID} =?"
+                    val phoneCursor =
+                        getApplication<Application>().contentResolver.query(
+                            uriPhone,
+                            null,
+                            selection,
+                            arrayOf(id),
+                            null
+                        )
+                    phoneCursor?.let {
+                        if (phoneCursor.count > 0) {
+                            Log.d("TAG", "getContacts: ${phoneCursor.count}")
+                            if (phoneCursor.moveToNext()) {
+                                val number =
+                                    phoneCursor.getString(
+                                        phoneCursor.getColumnIndex(
+                                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                                        )
+                                    )
+                                Log.d("TAG", "getContacts: $name $number ${phoneCursor.position}")
+                                when (phoneCursor.getInt(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE))) {
+                                    ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE -> {
+                                        val contact = ContactEntity(name = name, number = number)
+                                        contacts.add(contact)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    phoneCursor?.close()
+                }
+            }
+        }
+        cursor?.close()
+        saveState(contacts)
+    }
+}
