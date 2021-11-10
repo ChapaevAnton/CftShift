b47d505636a7851edad828193a045c0f67b80465
Chapaev Anton <virsus2009@gmail.com>
2021-11-10 20:00:10 +0700

    Homework 08.11.21 dependency_injection
    * refactoring scope

--- app/src/main/java/com/w4eret1ckrtb1tch/homework/MainActivity.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/homework/MainActivity.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import com.w4eret1ckrtb1tch.homework.di.ActivityComponent
 import com.w4eret1ckrtb1tch.homework.di.FragmentComponent
 import com.w4eret1ckrtb1tch.homework.presentation.ScreenFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var activityComponent: ActivityComponent
     lateinit var fragmentComponent: FragmentComponent
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        activityComponent = (application as App).appComponent.activityComponent().build()
+        val activityComponent = (application as App).appComponent.activityComponent().build()
         activityComponent.inject(this)
         fragmentComponent = (application as App).appComponent.fragmentComponent().build()
         super.onCreate(savedInstanceState)
