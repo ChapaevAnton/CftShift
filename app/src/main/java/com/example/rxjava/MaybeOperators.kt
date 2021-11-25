f7d717591630df81399564ef9370514889c8e908
ChapaevAnton <virsus2009@gmail.com>
2021-11-25 16:35:13 +0700

    homework 24.11.21 14_reactive_programming

--- app/src/main/java/com/example/rxjava/MaybeOperators.kt
+++ app/src/main/java/com/example/rxjava/MaybeOperators.kt
@@ -5,45 +5,45 @@ import io.reactivex.rxjava3.core.Single
 
 object MaybeOperators {
 
-	object Task1 {
+    object Task1 {
 
-		/**
-		 * Вернуть пустой Maybe, который мгновенно завершит работу.
-		 */
-		fun solve(): Maybe<String> =
-			TODO()
-	}
+        /**
+         * Вернуть пустой Maybe, который мгновенно завершит работу.
+         */
+        fun solve(): Maybe<String> =
+            Maybe.empty()
+    }
 
-	object Task2 {
+    object Task2 {
 
-		/**
-		 * Преобразовать результат получаемый из [worker] в Maybe поток.
-		 */
-		fun solve(worker: Worker): Maybe<String> =
-			TODO()
+        /**
+         * Преобразовать результат получаемый из [worker] в Maybe поток.
+         */
+        fun solve(worker: Worker): Maybe<String> =
+            TODO()
 
-		interface Worker {
+        interface Worker {
 
-			fun setResultListener(listener: (String?) -> Unit)
-		}
-	}
+            fun setResultListener(listener: (String?) -> Unit)
+        }
+    }
 
-	object Task3 {
+    object Task3 {
 
-		/**
-		 * Завершать поток, если в [source] будет отрицательное число.
-		 * Возвращать значение, если оно будет положительным.
-		 */
-		fun solve(source: Single<Int>): Maybe<Int> =
-			TODO()
-	}
+        /**
+         * Завершать поток, если в [source] будет отрицательное число.
+         * Возвращать значение, если оно будет положительным.
+         */
+        fun solve(source: Single<Int>): Maybe<Int> =
+            TODO()
+    }
 
-	object Task4 {
+    object Task4 {
 
-		/**
-		 * Если [first] поток не вернет значение, то переключить на [second] поток.
-		 */
-		fun solve(first: Maybe<Int>, second: Single<Int>): Single<Int> =
-			TODO()
-	}
+        /**
+         * Если [first] поток не вернет значение, то переключить на [second] поток.
+         */
+        fun solve(first: Maybe<Int>, second: Single<Int>): Single<Int> =
+            TODO()
+    }
 }
--- app/src/main/java/com/example/rxjava/SingleOperators.kt
+++ app/src/main/java/com/example/rxjava/SingleOperators.kt
@@ -2,104 +2,107 @@ package com.example.rxjava
 
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.kotlin.Singles
 
 object SingleOperators {
 
-	object Task1 {
+    object Task1 {
 
-		/**
-		 * Реализовать поток данных вида:
-		 * "Hello"
-		 *
-		 * Успешно завершить передачу данных.
-		 */
-		fun solve(): Single<String> =
-			TODO()
-	}
+        /**
+         * Реализовать поток данных вида:
+         * "Hello"
+         *
+         * Успешно завершить передачу данных.
+         */
+        fun solve(): Single<String> =
+            Single.just("Hello")
+    }
 
-	object Task2 {
+    object Task2 {
 
-		/**
-		 * Установить слушателя инициализации чата.
-		 * При успешной инициализации успешно завершать поток с передачей имени чата.
-		 * При фатальной ошибке инициализации завершать поток с ошибкой.
-		 */
-		fun solve(chat: Chat): Single<String> =
-			TODO()
+        /**
+         * Установить слушателя инициализации чата.
+         * При успешной инициализации успешно завершать поток с передачей имени чата.
+         * При фатальной ошибке инициализации завершать поток с ошибкой.
+         */
+        fun solve(chat: Chat): Single<String> =
+            TODO()
 
-		interface Chat {
+        interface Chat {
 
-			fun setInitListener(listener: MessageListener)
-		}
+            fun setInitListener(listener: MessageListener)
+        }
 
-		class MessageListener(
-			val onSuccess: (name: String) -> Unit,
-			val onFatalError: (Throwable) -> Unit,
-		)
-	}
+        class MessageListener(
+            val onSuccess: (name: String) -> Unit,
+            val onFatalError: (Throwable) -> Unit,
+        )
+    }
 
-	object Task3 {
+    object Task3 {
 
-		/**
-		 * Реализовать поток данных, который будет возвращать значение, получаемое из провайдера.
-		 * Провайдер при каждом вызове возвращает новое уникальное значение.
-		 * Сделать так, чтобы при повторной подписке возвращалось новое значение.
-		 */
-		fun solve(provider: DataProvider): Single<Int> =
-			TODO()
+        /**
+         * Реализовать поток данных, который будет возвращать значение, получаемое из провайдера.
+         * Провайдер при каждом вызове возвращает новое уникальное значение.
+         * Сделать так, чтобы при повторной подписке возвращалось новое значение.
+         */
+        fun solve(provider: DataProvider): Single<Int> =
+            Single.defer { Single.just(provider.get()) }
 
-		interface DataProvider {
+        interface DataProvider {
 
-			fun get(): Int
-		}
-	}
+            fun get(): Int
+        }
+    }
 
-	object Task4 {
+    object Task4 {
 
-		/**
-		 * Уменьшить значение из [source] на единицу.
-		 */
-		fun solve(source: Observable<Int>): Observable<Int> =
-			TODO()
-	}
+        /**
+         * Уменьшить значение из [source] на единицу.
+         */
+        fun solve(source: Observable<Int>): Observable<Int> =
+            source.map { it.minus(1) }
+    }
 
-	object Task5 {
+    object Task5 {
 
-		/**
-		 * [api] предоставляет методы для работы с пользователем.
-		 * create - создает нового пользователя и возвращает id.
-		 * get - возращает сущность пользователя по указанному id.
-		 *
-		 * Необходимо создать пользователя с указанным именем и вернуть сущность User.
-		 */
-		fun solve(name: String, api: UserApi): Single<User> =
-			TODO()
+        /**
+         * [api] предоставляет методы для работы с пользователем.
+         * create - создает нового пользователя и возвращает id.
+         * get - возращает сущность пользователя по указанному id.
+         *
+         * Необходимо создать пользователя с указанным именем и вернуть сущность User.
+         */
+        fun solve(name: String, api: UserApi): Single<User> =
+            Single.just(api).flatMap { itApi -> itApi.create(name).flatMap { id -> itApi.get(id) } }
 
-		interface UserApi {
 
-			fun create(name: String): Single<Long>
-			fun get(id: Long): Single<User>
-		}
+        interface UserApi {
 
-		data class User(val name: String, val id: Long)
-	}
+            fun create(name: String): Single<Long>
+            fun get(id: Long): Single<User>
+        }
 
-	object Task6 {
+        data class User(val name: String, val id: Long)
+    }
 
-		/**
-		 * Выполнить параллельно запросы id, name, age пользователя и вернуть сущность User.
-		 * Предполагается, что запросы getId(), getName(), getAge() уже выполняются на разных тредах.
-		 */
-		fun solve(userApi: UserApi): Single<User> =
-			TODO()
+    object Task6 {
 
-		interface UserApi {
+        /**
+         * Выполнить параллельно запросы id, name, age пользователя и вернуть сущность User.
+         * Предполагается, что запросы getId(), getName(), getAge() уже выполняются на разных тредах.
+         */
+        fun solve(userApi: UserApi): Single<User> =
+            Singles.zip(userApi.getId(), userApi.getName(), userApi.getAge())
+                .map { (id, name, age) -> User(id, name, age) }
 
-			fun getId(): Single<Long>
-			fun getName(): Single<String>
-			fun getAge(): Single<Int>
-		}
+        interface UserApi {
 
-		data class User(val id: Long, val name: String, val age: Int)
-	}
+            fun getId(): Single<Long>
+            fun getName(): Single<String>
+            fun getAge(): Single<Int>
+        }
+
+        data class User(val id: Long, val name: String, val age: Int)
+    }
 }
