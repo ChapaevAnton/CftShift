cb17118abff8a56afbb58d87a96d27379b3a6828
ChapaevAnton <virsus2009@gmail.com>
2021-11-26 17:20:59 +0700

    homework 24.11.21 14_reactive_programming

--- app/src/main/java/com/example/rxjava/CompletableOperators.kt
+++ app/src/main/java/com/example/rxjava/CompletableOperators.kt
@@ -6,72 +6,72 @@ import io.reactivex.rxjava3.core.Single
 
 object CompletableOperators {
 
-	object Task1 {
+    object Task1 {
 
-		/**
-		 * Вернуть Completable, который моментально успешно завершает работу.
-		 */
-		fun solve(): Completable =
-			TODO()
-	}
+        /**
+         * Вернуть Completable, который моментально успешно завершает работу.
+         */
+        fun solve(): Completable =
+            Completable.complete()
+    }
 
-	object Task2 {
+    object Task2 {
 
-		/**
-		 * Вернуть Completable, который моментально завершает работу с любой ошибкой.
-		 */
-		fun solve(): Completable =
-			TODO()
-	}
+        /**
+         * Вернуть Completable, который моментально завершает работу с любой ошибкой.
+         */
+        fun solve(): Completable =
+            Completable.error(Throwable("Error"))
+    }
 
-	object Task3 {
+    object Task3 {
 
-		/**
-		 * Преобразовать вызов блокирующей поток операции в Completable.
-		 */
-		fun solve(worker: Worker): Completable =
-			TODO()
+        /**
+         * Преобразовать вызов блокирующей поток операции в Completable.
+         */
+        fun solve(worker: Worker): Completable =
+            Completable.fromAction { worker.doWork() }
 
-		interface Worker {
+        interface Worker {
 
-			// Этот вызов блокирует поток
-			fun doWork()
-		}
-	}
+            // Этот вызов блокирует поток
+            fun doWork()
+        }
+    }
 
-	object Task4 {
+    object Task4 {
 
-		/**
-		 * Преобразовать [source] в Completable.
-		 */
-		fun solve(source: Single<String>): Completable =
-			TODO()
-	}
+        /**
+         * Преобразовать [source] в Completable.
+         */
+        fun solve(source: Single<String>): Completable =
+            Completable.fromSingle(source)
+    }
 
-	object Task5 {
+    object Task5 {
 
-		/**
-		 * Последовательно выполнить [first], [second] и [third].
-		 */
-		fun solve(first: Completable, second: Completable, third: Completable): Completable =
-			TODO()
-	}
+        /**
+         * Последовательно выполнить [first], [second] и [third].
+         */
+        fun solve(first: Completable, second: Completable, third: Completable): Completable =
+            Completable.concatArray(first.andThen(second).andThen(third))
+    }
 
-	object Task6 {
+    object Task6 {
 
-		/**
-		 * Переключить операторы, которые будут идти ниже по цепочке на [scheduler].
-		 */
-		fun solve(source: Completable, scheduler: Scheduler): Completable =
-			TODO()
-	}
+        /**
+         * Переключить операторы, которые будут идти ниже по цепочке на [scheduler].
+         */
+        fun solve(source: Completable, scheduler: Scheduler): Completable =
+            source.observeOn(scheduler)
+    }
 
-	object Task7 {
+    object Task7 {
 
-		/**
-		 * Переключить выполнение всей цепочки на [scheduler].
-		 */
-		fun solve(source: Completable, scheduler: Scheduler): Completable =
-			TODO()
-	}
+        /**
+         * Переключить выполнение всей цепочки на [scheduler].
+         */
+        fun solve(source: Completable, scheduler: Scheduler): Completable =
+            source.subscribeOn(scheduler)
+    }
 }
