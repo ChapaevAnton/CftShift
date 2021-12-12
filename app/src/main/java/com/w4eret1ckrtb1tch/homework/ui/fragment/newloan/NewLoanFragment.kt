d79ee80725d11595b2e0ccf5e5b07a178ce39357
Chapaev Anton <virsus2009@gmail.com>
2021-12-12 13:46:47 +0700

    final project 12.12.21
    * added set info new loan fragment

--- app/src/main/java/com/w4eret1ckrtb1tch/homework/ui/fragment/newloan/NewLoanFragment.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/homework/ui/fragment/newloan/NewLoanFragment.kt
@@ -5,7 +5,10 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.w4eret1ckrtb1tch.homework.R
 import com.w4eret1ckrtb1tch.homework.databinding.FragmentNewLoanBinding
+import com.w4eret1ckrtb1tch.homework.domain.entity.LoanConditions
+import com.w4eret1ckrtb1tch.homework.domain.entity.Result
 import com.w4eret1ckrtb1tch.homework.presentation.viewmodel.NewLoanViewModel
+import com.w4eret1ckrtb1tch.homework.ui.activity.MainActivity
 import com.w4eret1ckrtb1tch.homework.ui.fragment.BaseFragment
 
 class NewLoanFragment : BaseFragment<FragmentNewLoanBinding>(
@@ -17,9 +20,45 @@ class NewLoanFragment : BaseFragment<FragmentNewLoanBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.apply {
-            sendNewLoan.setOnClickListener {  }
+            sendNewLoan.setOnClickListener { createLoan() }
+        }
+        viewModel.getConditions.observe(viewLifecycleOwner) { resolveLoanConditions(it) }
+    }
+
+    private fun resolveLoanConditions(result: Result<LoanConditions>) {
+        when (result) {
+            is Result.Success -> {
+                setLoanConditions(result.value)
+            }
+            is Result.Failure -> {
+                (requireActivity() as MainActivity).showMessage(
+                    result.toString(),
+                    binding.root,
+                    null
+                )
+            }
+            is Result.Loading -> {
+                // TODO: 07.12.2021 Добавить индикатор загрузки
+            }
+        }
+    }
+
+    private fun setLoanConditions(loanConditions: LoanConditions) {
+        binding.apply {
+            percent.text = loanConditions.percent.toString()
+            period.text = loanConditions.period.toString()
+            loanConditions.maxAmount.toString().also { maxAmount.text = it }
+        }
+    }
+
+    private fun createLoan() {
+        binding.apply {
+            val firstName = firstName.editText?.text.toString()
+            val lastName = lastName.editText?.text.toString()
+            val phoneNumber = phoneNumber.editText?.text.toString()
+            val amount = amount.editText?.text.toString().toLong()
+            viewModel.createLoan(firstName, lastName, phoneNumber, amount)
         }
     }
 }
